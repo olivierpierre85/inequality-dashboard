60845da247504ef1bd85ed4340a9aaca25adba4a
Olivier <olivierpierre85@gmail.com>
2021-01-19 13:46:56 +0100

    add module to load image

--- inequality_dashboard/frontend/webpack.config.js
+++ inequality_dashboard/frontend/webpack.config.js
@@ -15,7 +15,16 @@ module.exports = {
         use: {
           loader: "babel-loader",
         },
-      },
+      },{
+        test: /\.css$/,
+        use: {
+          loader: "style-loader!css-loader",
+        },
+      }, {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        use: {
+          loader: 'url-loader?limit=100000' }
+        },
     ],
   },
   optimization: {
