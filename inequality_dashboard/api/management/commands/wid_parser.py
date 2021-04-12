d404b4211317b4850e18deec28ff27f168fb349a
Olivier Pierre <olivierpierre85@gmail.com>
2021-04-12 12:06:59 +0200

    first test parser for chart 1

--- inequality_dashboard/api/management/commands/wid_parser.py
+++ inequality_dashboard/api/management/commands/wid_parser.py
@@ -11,13 +11,19 @@ class Command(BaseCommand):
         with open('/home/olpi/projects/inequality-dashboard/WID_DATA/WID_data_BE.csv') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=';')
             line_count = 0
-            # TODO empty previous indicators
+            # empty previous indicators
+            Indicator.objects.all().delete()
+
+            # list of all variables needed for charts
+            variables_list = ['sptinc992j']
+            percentiles_list = ['p90p100','p0p50','p50p90','p99p100']
+
             for row in csv_reader:
                 if line_count == 0:
                     print(f'Column names are {", ".join(row)}')
                     line_count += 1
                 else:
-                    if row[1] == "acainc992j":
+                    if row[1] in variables_list and row[2] in percentiles_list :
                         q = Indicator(
                                 year = row[3],
                                 country = Country.objects.get(code=row[0]),
