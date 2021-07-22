d20fdebec339275f0a3c3daab1478038b4b866a1
Olivier Pierre <olivierpierre85@gmail.com>
2021-07-22 17:03:30 +0200

    build works, not dev mode

--- inequality_dashboard/vue_frontend/vue.config.js
+++ inequality_dashboard/vue_frontend/vue.config.js
@@ -1,10 +1,16 @@
-  module.exports = {
-    chainWebpack: config => {
-      config
-        .plugin('html')
-        .tap(args => {
-          args[0].template = './templates/vue_frontend/index.html'
-          return args
-        })
-    }
+//   module.exports = {
+//     chainWebpack: config => {
+//       config
+//         .plugin('html')
+//         .tap(args => {
+//           args[0].template = './templates/vue_frontend/index.html'
+//           return args
+//         })
+//     }
+//   }
+const path = require("path");
+
+module.exports = {
+    outputDir: path.resolve(__dirname, "./templates/vue_frontend"),
+    assetsDir: "../../static"
   }
