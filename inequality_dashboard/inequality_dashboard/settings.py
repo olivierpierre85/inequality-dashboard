1fba91d8232f2389199d55dd44b2f053ab9cb7fe
Olivier Pierre <olivierpierre85@gmail.com>
2021-07-22 16:25:38 +0200

    Install basis for vue integration

--- inequality_dashboard/inequality_dashboard/settings.py
+++ inequality_dashboard/inequality_dashboard/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
-    'frontend'
+    'frontend',
+    'vue_frontend'
 ]
 
 MIDDLEWARE = [
--- inequality_dashboard/inequality_dashboard/urls.py
+++ inequality_dashboard/inequality_dashboard/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('api/',include('api.urls')),
     path('admin/', admin.site.urls),
-    path('', include('frontend.urls'))
+    #path('', include('frontend.urls'))
+    path('', include('vue_frontend.urls'))
 ]
