150adfded9853f7b9a880d34eaf798709560ac8a
Olivier Pierre <olivierpierre85@gmail.com>
2021-07-23 10:45:26 +0200

    env config vue frontend

--- inequality_dashboard/vue_frontend/vue.config.js
+++ inequality_dashboard/vue_frontend/vue.config.js
@@ -1,16 +1,6 @@
-//   module.exports = {
-//     chainWebpack: config => {
-//       config
-//         .plugin('html')
-//         .tap(args => {
-//           args[0].template = './templates/vue_frontend/index.html'
-//           return args
-//         })
-//     }
-//   }
 const path = require("path");
-
+//Uncommment before production?
 module.exports = {
-    outputDir: path.resolve(__dirname, "./templates/vue_frontend"),
-    assetsDir: "../../static"
+    outputDir: path.resolve(__dirname, process.env.OUTPUT_DIR),
+    assetsDir: process.env.ASSETS_DIR
   }
